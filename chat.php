52accc790fe2a9e3303e6b047b195e817b6cc8d9
Daniel <voje@live.no>
2019-03-15 20:40:42 +0100

    Worked on the friend-chat and PHPdoc on Db_object
    The friend-chat is now up and ready to be worked on more, currently used the controller to what room as a radio-button behind the friend, clicking on it will give you the chat based on the friends id at the moment.
    This will be changed later to both the players id with a # in the middle to make it unique. 
    
    Started the great documenting by doing the Db_object one , plan to finish the classes tomorrow.

--- chat.php
+++ chat.php
@@ -4,7 +4,12 @@
 
 
 
-<?php include("includes/views/chat_content.php"); ?>
+<?php 
+
+// Calls here on the chat_content file that will hold the chat.
+include("includes/views/chat_content.php"); 
+
+?>
 
 
 
