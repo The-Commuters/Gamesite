ba34f8fcf3aff53c12d63fc0df975f54c09f8e39
Unknown <markusfenes@gmail.com>
2019-03-29 20:54:00 +0100

    Some more work on the password reset
    The reset code now gets stored in the database and gets logged as "password reset", for use when the user click on the link in the email
    
    ** To do **
    
    * Figure out in what order they are to be run
    * Make a password reset page 
    * Save the new password

--- includes/classes/email.php
+++ includes/classes/email.php
@@ -6,13 +6,13 @@ class Email extends User{
 	// Denne styrer hvilken mail som eposten blir sent fra 
 	private static $fromEmail = "no-reply@cm-games.com" ;
 
-	protected static $db_table = "email";
+	protected static $db_table = "email_codes";
 
-	public $id;
+
 	public $user_id;
 	public $reset_code;
 
-	protected static $db_table_fields = array('user_id','reset_code' );
+	protected static $db_table_fields = array('id','reset_code' );
 
 
 	public static function mail_Sender($to, $subject, $txt){
@@ -32,7 +32,7 @@ class Email extends User{
 
 	
 	public static function send_ActivationMail($to, $user_name, $verify_code ){
- 
+
 		$subject = "Hello " . $user_name . " Please activate your account at CM Games";
 
 		$txt = "Please press the link below to activate your new account at CM Games " . "http://localhost/gamesite/activate.php?code=" . $verify_code . " Thank you for registering. ";
@@ -52,10 +52,8 @@ class Email extends User{
 	 * 
 	 */
 
-
-
 	public static function send_Password_ResetMail($to, $user_name, $reset_code ){
- 
+
 		$subject = "Hello " . $user_name . " Here is your password reset";
 
 		$txt = "Please press the link below to reset your password for your account at CM Games " . "http://localhost/gamesite/activate.php?reset_code=" . $reset_code;
@@ -64,40 +62,39 @@ class Email extends User{
 		self::mail_sender($to, $subject, $txt);
 	}
 
-	public static function create_reset_code($email){
+	public function create_reset_code($email){
 
 		global $database;
+		
 		// Tar bort eventuele ting som ikke skal være med i stringen
 		$email = $database->escape_string($email);
 		
 		// Finner brukeren med denne eposten
 		$in = User::find_user_by_email($email);
 
+		// Legger bruker objektet inn i variabelen $user og hopper til første posisjon 		
 		$user = array_shift($in);
+		// Oppretter et nytt email objekt 
+		$user_email = new Email();
+		// Setter variablene id og lager en unik kode for reseting
+		$user_email->id = $user->id;
 
-		var_dump($user->email);
+		$reset_code = md5($user->email . microtime());
+	
+		$user_email->reset_code = $reset_code;
+
+		// Lagrer alt dette i databasen ved bruk av den overrida create metoden lengere nede
+		$user_email->create();
 
 		
-
-	 	$reset_code = md5($user->email . microtime());
-
-	 	$user_email = new email();	
-
-	 	$user_email->user_id  = $user->id;
-
-	 	$user_email->reset_code = $reset_code;
-
-	 	$user_email->create();
-
-
-
-	 	//var_dump($reset_code);
-
-	 	return $reset_code;
+		return $reset_code;
 		
 
 		
 	}
+
+	
+
 
 	public static function find_user_by_reset_code($code){
 
@@ -110,6 +107,46 @@ class Email extends User{
 		
 		// retunerer det database objektet som blir funnet 
 		return static ::find_by_query($sql); 
+	}
+
+
+
+
+
+	/**
+	 * This is the clas that create database-rows for any of the objects that have
+	 * their own object-classes, this checks and stores rows in the database to be 
+	 * used later, and is like the other classes abstract so that it works for all
+	 * the classes. It also uses the log_user_activity to create a new row in the 
+	 * user activity folder.
+	 *
+	 * @return true if the object is stored in the database, false if not.
+	 */
+	public function create() {
+		
+		global $database;
+		global $session;
+
+		$properties = $this->clean_properties();
+
+		$sql = "INSERT INTO " . static::$db_table . " (" . implode(",", array_keys($properties)) . ")";
+		$sql .= "VALUES ('" . implode("','", array_values($properties)) . "')";
+
+		if ($database->query($sql)) {
+			
+		
+			$user_id = $this->id;
+
+			$log = $this->log_user_activity("password reset", $user_id, $this->id, static::$db_table);
+
+			return true;
+
+		} else {
+
+			return false;
+
+		}
+
 	}
 
 }
