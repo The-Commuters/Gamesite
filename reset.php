9cf416a27af2481ae5e2c1c8014832fafd7d3810
Unknown <markusfenes@gmail.com>
2019-04-05 14:51:01 +0200

    Added a reset.php
    This will handle the password reset request info entered by the user that wants their password reset.
    
    The user can enter their email in to the single field and press submit and they will get a email, if they are in our system.

---
+++ reset.php
@@ -0,0 +1,84 @@
+<?php require_once("includes/init.php") ?>
+<?php include("includes/views/header.php"); ?>
+
+<!-- <?php if ($session->is_signed_in()) {redirect("profile.php");} ?> -->
+
+<?php 
+
+if (isset($_POST['submit'])) {
+
+	// Fjerner eventuelle tagger og stringer som kan skade databasen	 
+	$email = trim($_POST['email']);
+
+	//Putter brukerens email inni metoden create_reset_code
+	$error_array = Email::create_reset_code($email);
+
+
+	if (empty($error_array)) {
+
+		$the_message="Email sent";
+		
+	} else {
+
+		$the_message = "Your information is incorrect.";
+	}
+
+} else {
+		$email          = "";
+		$the_message 	= "";
+		$error_array    = "";
+	}
+
+?>
+
+<div>
+<h4><?php echo $the_message; ?></h4>
+
+<!-- 
+Åpner en form for slik at brukeren kan skrive inne sin epost for å så kunne få til sent en passord tilbakestillingsepost
+
+-->
+	
+<form id="reset-password" action="" method="post">
+
+<div class="">
+	<label>Email</label>
+	<input type="email" name="email" value="<?php echo htmlentities($email); ?>" required  >
+
+</div>
+
+ <?php 
+
+/*Hvis det er meldinger i errorarray blir de skrivt ut til brukeren med ny linje*/
+
+if (!empty($error_array)) {
+	foreach ($error_array as $error_message) {
+		echo $error_message . "<br>";
+
+	}
+}
+
+?>
+
+<!-- Div tagger for knapper -->
+<div class="">
+<input type="submit" name="submit" value="Submit">
+
+</div>
+
+</form>
+
+</div>
+
+<?php 
+
+	
+	//Email::create_reset_code("newuser@localhost.com");
+
+	//Email::send_Password_resetMail("newuser@localhost", "Mark", "testcode");
+
+?>
+
+
+
+<?php include("includes/views/footer.php"); ?>
