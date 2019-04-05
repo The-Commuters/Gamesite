76d397a2f3665201a8d10e7b2582bf9b19ab4347
Unknown <markusfenes@gmail.com>
2019-04-05 16:40:19 +0200

    Activate can find the user by reset_code
    But I am wondering where should this method go?
    It shares similarities with activate user but it also does mostly different things

--- activate.php
+++ activate.php
@@ -1,6 +1,7 @@
 <?php require_once("includes/init.php") ?>
 <?php include("includes/views/header.php"); ?>
 
+
 <!-- <?php if ($session->is_signed_in()) {redirect("profile.php");} ?> -->
 
 <?php 
@@ -18,16 +19,37 @@ if (isset($_GET["code"])){
 
 	// henter inn koden og gir den vidre til aktiverings metoden 
 	User::activate_user($code);
+}
+elseif (isset($_GET["reset_code"])) {
+	$reset_code = ($_GET["reset_code"]);
 
-}else{
+	// henter inn koden og gir den vidre til aktiverings metoden 
+	$code = Email::find_user_by_reset_code($reset_code);
+	
+	$shifted_code = array_shift($code);
+	$user_id = $shifted_code->id;
+
+
+
+	$user = User::find_by_id($user_id);
+	
+	var_dump($user);
+
+	
+
+
+}
+else{
 	echo "Activation could not be completed \n" ;
 }
+
 	
 	//Email::create_reset_code("newuser@localhost.com");
 
 	//Email::send_Password_resetMail("newuser@localhost", "Mark", "testcode");
 
 ?>
+
 
 
 <script src="js/functions.js"></script>
