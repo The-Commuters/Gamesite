5c7da9db8ab0be40999446ba60dd77af5ddb0304
Unknown <markusfenes@gmail.com>
2019-04-09 18:00:59 +0200

    Moved password resetting in to reset.php
    It makes more sense to have the passsword reseting in the reset.php instead of the activate .php since it does not activate anything

--- activate.php
+++ activate.php
@@ -20,25 +20,7 @@ if (isset($_GET["code"])){
 	// henter inn koden og gir den vidre til aktiverings metoden 
 	User::activate_user($code);
 }
-elseif (isset($_GET["reset_code"])) {
-	$reset_code = ($_GET["reset_code"]);
 
-	// henter inn koden og gir den vidre til aktiverings metoden 
-	$code = Email::find_user_by_reset_code($reset_code);
-	
-	$shifted_code = array_shift($code);
-	$user_id = $shifted_code->id;
-
-
-
-	$user = User::find_by_id($user_id);
-	
-	var_dump($user);
-
-	
-
-
-}
 else{
 	echo "Activation could not be completed \n" ;
 }
