a4ce5a7cb4769f22c566384760628188e6304f03
David Naist Øvernes <David.Naist@gmail.com>
2019-04-09 19:28:53 +0200

    A lot of changes, mostly scss tho.

--- about.php
+++ about.php
@@ -6,9 +6,62 @@
 
 $page = basename(__FILE__, '.php');
 include("includes/views/header.php");
-
-
-
-include("includes/views/footer.php"); 
-
 ?>
+
+<main class="about">
+    <div class="hero">
+        <div class="hero-image" style="background-image: url(assets/img/type.png)"></div>
+    </div>
+
+    <div class="row">
+        <div class="text-container">
+            <h2>Our story</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus quod neque laudantium nostrum numquam adipisci deleniti quasi dolorum, voluptas ducimus voluptate qui molestias, maiores porro? Vel temporibus expedita fuga sed.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus quod neque laudantium nostrum numquam adipisci deleniti quasi dolorum, voluptas ducimus voluptate qui molestias, maiores porro? Vel temporibus expedita fuga sed.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus quod neque laudantium nostrum numquam adipisci deleniti quasi dolorum, voluptas ducimus voluptate qui molestias, maiores porro? Vel temporibus expedita fuga sed.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus quod neque laudantium nostrum numquam adipisci deleniti quasi dolorum, voluptas ducimus voluptate qui molestias, maiores porro? Vel temporibus expedita fuga sed.</p>
+        </div>
+        <img src="assets/img/about/chat.svg" alt="About work" class="art">
+    </div>
+
+    <div class="fancy">
+    <div class="row">
+        <img src="assets/img/about/work.svg" alt="About work" class="art">
+        <div class="text-container">
+            <h2>Our work ethic</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus quod neque laudantium nostrum numquam adipisci deleniti quasi dolorum, voluptas ducimus voluptate qui molestias, maiores porro? Vel temporibus expedita fuga sed.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus quod neque laudantium nostrum numquam adipisci deleniti quasi dolorum, voluptas ducimus voluptate qui molestias, maiores porro? Vel temporibus expedita fuga sed.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus quod neque laudantium nostrum numquam adipisci deleniti quasi dolorum, voluptas ducimus voluptate qui molestias, maiores porro? Vel temporibus expedita fuga sed.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus quod neque laudantium nostrum numquam adipisci deleniti quasi dolorum, voluptas ducimus voluptate qui molestias, maiores porro? Vel temporibus expedita fuga sed.</p>
+        </div>
+    </div>
+    </div>
+
+    <div class="row">
+        <div class="text-container">
+            <h2>Our Team</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus quod neque laudantium nostrum numquam adipisci deleniti quasi dolorum, voluptas ducimus voluptate qui molestias, maiores porro? Vel temporibus expedita fuga sed.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus quod neque laudantium nostrum numquam adipisci deleniti quasi dolorum, voluptas ducimus voluptate qui molestias, maiores porro? Vel temporibus expedita fuga sed.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus quod neque laudantium nostrum numquam adipisci deleniti quasi dolorum, voluptas ducimus voluptate qui molestias, maiores porro? Vel temporibus expedita fuga sed.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus quod neque laudantium nostrum numquam adipisci deleniti quasi dolorum, voluptas ducimus voluptate qui molestias, maiores porro? Vel temporibus expedita fuga sed.</p>
+        </div>
+        <img src="assets/img/about/team.svg" alt="About work" class="art">
+    </div>
+
+    <div class="fancy">
+    <div class="row">
+        <img src="assets/img/about/windows.svg" alt="About work" class="art">
+        <div class="text-container">
+            <h2>Our Projects</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus quod neque laudantium nostrum numquam adipisci deleniti quasi dolorum, voluptas ducimus voluptate qui molestias, maiores porro? Vel temporibus expedita fuga sed.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus quod neque laudantium nostrum numquam adipisci deleniti quasi dolorum, voluptas ducimus voluptate qui molestias, maiores porro? Vel temporibus expedita fuga sed.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus quod neque laudantium nostrum numquam adipisci deleniti quasi dolorum, voluptas ducimus voluptate qui molestias, maiores porro? Vel temporibus expedita fuga sed.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus quod neque laudantium nostrum numquam adipisci deleniti quasi dolorum, voluptas ducimus voluptate qui molestias, maiores porro? Vel temporibus expedita fuga sed.</p>
+        </div>
+    </div>
+    </div>
+
+</main>
+
+
+<?php include("includes/views/footer.php"); ?>
--- contact.php
+++ contact.php
@@ -6,9 +6,123 @@
 
 $page = basename(__FILE__, '.php');
 include("includes/views/header.php");
-
-
-
-include("includes/views/footer.php"); 
-
 ?>
+<main class="games">
+
+<!--
+<div class="rating">
+    <i class="fas fa-fw fa-star"></i>
+    <i class="fas fa-fw fa-star"></i>
+    <i class="fas fa-fw fa-star"></i>
+    <i class="fas fa-fw fa-star"></i>
+    <i class="fas fa-fw fa-star"></i>
+</div>
+-->
+    <div class="category">
+        <header>
+            <div class="category-title">Popular</div>
+
+            <form class="category-searchbar">
+                <input type="text" placeholder="Find a game" class="bar">
+                <div class="search">
+                    <i class="fas fa-fw fa-search"></i>
+                </div>
+            </form>
+
+            <div class="sort">
+                <i class="fas fa-fw fa-sort"></i>
+            </div>
+        </header>
+
+        <div class="cards">
+            <a href="" class="card">
+                <div class="card-image" style="background-image: url(assets/img/game.jpg)">
+                    <div class="card-image-content">
+                        <div>
+                            <div class="title">Titanrise</div>
+                            <div class="author">Titandev</div>
+                        </div>
+                        <div class="rating"><i class="fas fa-fw fa-heart"></i><span class="rating-placement">#1</span></div>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <section class="scroller">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat accusantium voluptate in impedit ab? Dolores ratione ducimus veritatis aliquam libero vel! Quas sint quam placeat nesciunt sit suscipit, non ipsam!</section>
+
+                    <footer>
+                        <div class="chip -red">Action</div>
+                        <div class="chip -teal">Adventure</div>
+                    </footer>
+                </div>
+            </a>
+
+            <a href="" class="card">
+                <div class="card-image" style="background-image: url(assets/img/game.jpg)">
+                    <div class="card-image-content">
+                        <div>
+                            <div class="title">Titanrise</div>
+                            <div class="author">Titandev</div>
+                        </div>
+                        <div class="rating"><i class="fas fa-fw fa-heart"></i><span class="rating-placement">#2</span></div>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <section class="scroller">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat accusantium voluptate in impedit ab? Dolores ratione ducimus veritatis aliquam libero vel! Quas sint quam placeat nesciunt sit suscipit, non ipsam!</section>
+
+                    <footer>
+                        <div class="chip -pink">Romance</div>
+                        <div class="chip -green">Rougelike</div>
+                    </footer>
+                </div>
+            </a>
+
+            <a href="" class="card">
+                <div class="card-image" style="background-image: url(assets/img/game.jpg)">
+                    <div class="card-image-content">
+                        <div>
+                            <div class="title">Titanrise</div>
+                            <div class="author">Titandev</div>
+                        </div>
+                        <div class="rating"><i class="fas fa-fw fa-heart"></i><span class="rating-placement">#4</span></div>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <section class="scroller">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat accusantium voluptate in impedit ab? Dolores ratione ducimus veritatis aliquam libero vel! Quas sint quam placeat nesciunt sit suscipit, non ipsam!</section>
+
+                    <footer>
+                        <div class="chip -orange">Simulation</div>
+                        <div class="chip -cyan">Strategy</div>
+                    </footer>
+                </div>
+            </a>
+
+            <a href="" class="card">
+                <div class="card-image" style="background-image: url(assets/img/game.jpg)">
+                    <div class="card-image-content">
+                        <div>
+                            <div class="title">Titanrise</div>
+                            <div class="author">Titandev</div>
+                        </div>
+                        <div class="rating"><i class="fas fa-fw fa-heart"></i><span class="rating-placement">#4</span></div>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <section class="scroller">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat accusantium voluptate in impedit ab? Dolores ratione ducimus veritatis aliquam libero vel! Quas sint quam placeat nesciunt sit suscipit, non ipsam!</section>
+
+                    <footer>
+                        <div class="chip -blue">Sport</div>
+                        <div class="chip -indigo">Idle</div>
+                        <div class="chip -purple">fantasy</div>
+                    </footer>
+                </div>
+            </a>
+        </div>
+    </div>
+
+
+</main>
+
+<?php include("includes/views/footer.php"); ?>
