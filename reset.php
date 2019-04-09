2106829d21a0806900f20c2085a76c1ca7d064b4
Unknown <markusfenes@gmail.com>
2019-04-09 18:24:59 +0200

    Added password resetting in to reset.php
    See previous commit for explanation

--- reset.php
+++ reset.php
@@ -72,7 +72,27 @@ if (!empty($error_array)) {
 
 <?php 
 
+
+
+if (isset($_GET["reset_code"])) {
+
+	$reset_code = ($_GET["reset_code"]);
+
+	// henter inn koden og gir den vidre til aktiverings metoden 
+	$code = Email::find_user_by_reset_code($reset_code);
 	
+	$shifted_code = array_shift($code);
+	$user_id = $shifted_code->id;
+
+	$user = User::find_by_id($user_id);
+	
+	//var_dump($user);
+
+}
+else{
+
+}
+
 	//Email::create_reset_code("newuser@localhost.com");
 
 	//Email::send_Password_resetMail("newuser@localhost", "Mark", "testcode");
