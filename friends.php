f4d9aeb50b8a8dfbd13531db31c144b51a56c959
David Naist Øvernes <David.Naist@gmail.com>
2019-04-10 20:54:43 +0200

    Ny venneliste
    La til en fin venneliste uten javacsript for å fjerne personen når du trykker på remove friend da, så det må lages, men det er veldig (hvis ikke aakuratt) den samme koden som jeg brukte for den mindre chat lista burde funke

--- friends.php
+++ friends.php
@@ -17,6 +17,101 @@ if (!isset($_GET['f'])) {
 
 ?>
 
+<main class="friendlist">
+    <h1 class="friendlist-title">Friends</h1>
+
+    <div class="friendlist-container">
+        <div class="friend">
+            <div class="friend-info">
+                <div style="background-image: url(assets/img/avatar.jpg)" class="avatar -s"></div>
+                <div class="username">Ripmoff</div>
+            </div>
+
+            <div class="friend-status">
+                <div class="status -active"></div>
+                <div class="description">Online</div>
+            </div>
+
+            <div class="friend-actions">
+                <a href="chat.php" class="action">
+                    <i class="fas fa-fw fa-comment-alt"></i>
+                </a>
+
+                <div class="action -delete">
+                    <i class="fas fa-fw fa-user-times"></i>
+                </div>
+            </div>
+        </div>
+
+        <div class="friend">
+            <div class="friend-info">
+                <div style="background-image: url(assets/img/avatar2.jpg)" class="avatar -s"></div>
+                <div class="username">Daniel</div>
+            </div>
+
+            <div class="friend-status">
+                <div class="status -away"></div>
+                <div class="description">Idle</div>
+            </div>
+
+            <div class="friend-actions">
+                <a href="chat.php" class="action">
+                    <i class="fas fa-fw fa-comment-alt"></i>
+                </a>
+
+                <div class="action -delete">
+                    <i class="fas fa-fw fa-user-times"></i>
+                </div>
+            </div>
+        </div>
+
+        <div class="friend">
+            <div class="friend-info">
+                <div style="background-image: url(assets/img/avatar3.jpg)" class="avatar -s"></div>
+                <div class="username">Mafen</div>
+            </div>
+
+            <div class="friend-status">
+                <div class="status -busy"></div>
+                <div class="description">Do Not Disturb</div>
+            </div>
+
+            <div class="friend-actions">
+                <a href="chat.php" class="action">
+                    <i class="fas fa-fw fa-comment-alt"></i>
+                </a>
+
+                <div class="action -delete">
+                    <i class="fas fa-fw fa-user-times"></i>
+                </div>
+            </div>
+        </div>
+
+        <div class="friend">
+            <div class="friend-info">
+                <div style="background-image: url(assets/img/avatar4.jpg)" class="avatar -s"></div>
+                <div class="username">Harimir</div>
+            </div>
+
+            <div class="friend-status">
+                <div class="status"></div>
+                <div class="description">Offline</div>
+            </div>
+
+            <div class="friend-actions">
+                <a href="chat.php" class="action">
+                    <i class="fas fa-fw fa-comment-alt"></i>
+                </a>
+
+                <div class="action -delete">
+                    <i class="fas fa-fw fa-user-times"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</main>
+
 
 
 
